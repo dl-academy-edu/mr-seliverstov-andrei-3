4331b39465fbee621a258ffec496bed630f55be6
Uber1337mensch <seliverstovandrey88@mail.ru>
2020-09-30 00:37:43 +0400

    1 & 2 homework

---
+++ src/pages/Lesson1/index.js
@@ -0,0 +1,10 @@
+let fName = prompt("Введите имя");
+let sName = prompt("Введите фамилию");
+let age = prompt("Введите возраст");
+let user = {
+    имя: fName,
+    фамилия: sName,
+    возраст: age,
+};
+
+console.log(user);
