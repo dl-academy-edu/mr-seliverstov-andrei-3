d02548b2a74046d113fe61a6a89de3e22c6a678e
Uber1337mensch <seliverstovandrey88@mail.ru>
2020-10-27 02:21:25 +0400

    Валидация форм

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -227,3 +227,235 @@
     })
     
 })();
+
+// Form validation
+
+function checkEmail(email) {
+    return email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+}
+
+function checkPhone(telephone) {
+    return telephone.match(/^(\s*)?(\+)?([-_():=+]?\d[- _():=+]?){10,14}(\s*)?$/);
+}
+
+function inputSetInvalid(input) {
+    if(input.hasAttribute("is-invalid")) {
+        return
+    }
+    input.setAttribute("is-invalid", "");
+    function handel() {
+        input.classList.remove("is-invalid");
+        input.removeAttribute("is-invalid");
+    }
+    input.classList.add("is-invalid");
+    input.addEventListener("input", handel);
+}
+
+function inputSetInvalidFeedback(input, error) {
+    if(input.hasAttribute("invalid-feedback")) {
+        return
+    }
+    input.setAttribute("invalid-feedback", "");
+    function handel() {
+        message.remove();
+        input.removeEventListener("input", handel);
+        input.removeAttribute("invalid-feedback");
+    }
+    const message = document.createElement("span");
+    message.classList.add("invalid-feedback");
+    message.innerText = error;
+    input.insertAdjacentElement("afterend", message)
+    input.addEventListener("input", handel);
+}
+
+function inputSetNormal(input) {
+    if(input.hasAttribute("is-normal")) {
+        return
+    }
+    input.setAttribute("is-normal", "");
+    function handel() {
+        input.classList.remove("is-normal");
+        input.removeAttribute("is-normal");
+    }
+    input.classList.add("is-normal");
+    input.addEventListener("input", handel);
+}
+
+function inputSetNormalFeedback(input) {
+    if(input.hasAttribute("normal-feedback")) {
+        return
+    }
+    input.setAttribute("normal-feedback", "");
+    function handel() {
+        message.remove();
+        input.removeEventListener("input", handel);
+        input.removeAttribute("normal-feedback");
+    }
+    const message = document.createElement("span");
+    message.classList.add("normal-feedback");
+    message.innerText = "All right";
+    input.insertAdjacentElement("afterend", message)
+    input.addEventListener("input", handel);
+}
+
+function setFormError(form, errors) {
+    let inputs = form.querySelectorAll("input");
+    for (let input of inputs) {
+        if(errors[input.name]) {
+            inputSetInvalid(input);
+            inputSetInvalidFeedback(input, errors[input.name]);
+        }
+        else {
+            inputSetNormal(input);
+            inputSetNormalFeedback(input);
+        }
+    }
+    let textareas = form.querySelectorAll("textarea");
+    for (let textarea of textareas) {
+        if(errors[textarea.name]) {
+            inputSetInvalid(textarea);
+            inputSetInvalidFeedback(input, errors[textarea.name]);
+        }
+        else {
+            inputSetNormal(input);
+            inputSetNormalFeedback(input);
+        }
+    }
+}
+
+function getFormData(form, data={}) {
+    let inputs = form.querySelectorAll("input");
+    for (let input of inputs) {
+        switch(input.type) {
+            case "radio":
+                if(input.checked) {
+                    data[input.name] = input.value;
+                }
+                break;
+            case "checkbox":
+                if(!data[input.name]) {
+                    data[input.name] = [];
+                }
+                if(input.checked) {
+                    if(data[input.name]) {                    
+                        data[input.name].push(input.value);
+                    }
+                }
+                break;
+            case "file":
+                data[input.name] = input.files;
+                break;
+            default:
+                data[input.name] = input.value;
+                break;
+        }
+    }
+    let textareas = form.querySelectorAll("textarea");
+    for (let textarea of textareas) {
+        data[textarea.name] = textarea.value;
+    }
+    return data;
+}
+
+(function() {
+    const form = document.forms["modal-register"];
+    console.log("modal-form =", form);
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const data = getFormData(event.target);
+        const errors = validateData(data);
+        if(Object.keys(errors).length) {
+            setFormError(form, errors);
+        }
+        console.log(errors);
+    });
+
+    function validateData(data, errors = {}) {
+        if(!checkEmail(data.email)) {
+            errors.email = "Введен неверный адрес почты";
+        }        
+        if(!data.name) {
+            errors.name = "Введите имя";
+        }
+        if(!data.surname) {
+            errors.surname = "Введите фамилию";
+        }
+        if(data.password.length < 8) {
+            errors.password = "Пароль слишком короткий";
+        }
+        if((data.password !== data["repeat-password"]) || (data["repeat-password"] === "")) {
+            errors["repeat-password"] = "Введен неправильный пароль";
+        }
+        if(data.location === "") {
+            errors.location = "Укажите адрес местоположения";
+        }  
+        if(data.age === "") {
+            errors.age = "Укажите возраст";
+        }  
+        if(data.consent[0] !== "consent") {
+            errors.consent = "Необходимо дать согласие";
+        }
+        return errors;
+    }
+})();
+
+(function() {
+    const form = document.forms["modal-sign"];
+    console.log("modal-form =", form);
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const data = getFormData(event.target);
+        const errors = validateData(data);
+        if(Object.keys(errors).length) {
+            setFormError(form, errors);
+        }
+        console.log(errors);
+    });
+
+    function validateData(data, errors = {}) {
+        if(!checkEmail(data.email)) {
+            errors.email = "Введен неверный адрес почты";
+        } 
+        if(data.password.length < 8) {
+            errors.password = "Пароль слишком короткий";
+        }
+        return errors;
+    }
+})();
+
+(function() {
+    const form = document.forms["modal-message"];
+    console.log("modal-form =", form);
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const data = getFormData(event.target);
+        const errors = validateData(data);
+        if(Object.keys(errors).length) {
+            setFormError(form, errors);
+        }
+        console.log(errors);
+    });
+
+    function validateData(data, errors = {}) {       
+        if(!data.name) {
+            errors.name = "Введите имя";
+        }
+        if(!data["message-subject"]) {
+            errors["message-subject"] = "Введите тему сообщения";
+        }
+        if(!data.email) {
+            errors.email = "Введите адрес почты";
+        } 
+        if(!data["mobile-phone"]) {
+            errors["mobile-phone"] = "Введите номер телефона";
+        }
+        if(data.consent[0] !== "consent") {
+            errors.consent = "Необходимо дать согласие";
+        }
+        return errors;
+    }
+})();
+
